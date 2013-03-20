6e30135c00028414fde8910d9bfa11a8284028f9
Brad Milne <brad.milne@getelevate.com>
2013-03-20 18:07:38 -0400

    deleted basic rails homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Personal::Application.routes.draw do
-  get "content/index"
+  root :to => "content/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
